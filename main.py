b7584487d809cb3b868f6a432fba22727bde8835
timurenk0 <zheltenkovtimur@gmail.com>
2025-12-16 13:44:03 +0100

    Added model training script to the main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from download_datasets import download_datasets
 from preprocess_data import preprocess_data
+from train_model import prepare_datasets, train_model
 
 
 if __name__ == "__main__":
@@ -12,3 +13,6 @@ if __name__ == "__main__":
     preprocess_data("datasets/raw/train.tsv").to_csv("datasets/train_preprocessed.csv", index=False)
     preprocess_data("datasets/raw/valid.tsv").to_csv("datasets/valid_preprocessed.csv", index=False)
 
+    # Train model
+    tokenized_datasets, tokenizer = prepare_datasets("datasets/train_preprocessed.csv", "datasets/valid_preprocessed.csv")
+    train_model(tokenized_datasets, tokenizer)
